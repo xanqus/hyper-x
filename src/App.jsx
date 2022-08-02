a04195d625e664e73f6330539182f1a03b8a8ddc
xanqus <sbk98031816@gmail.com>
2022-08-02 10:11:07 +0900

    check jwt expired

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useRecoilValue, useResetRecoilState } from "recoil";
 import AuthRoutes from "./pages/auth/AuthRoutes";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,32 +10,25 @@ const About = lazy(() => import("./pages/About"));
 const Users = lazy(() => import("./pages/Users"));
 
 const App = () => {
-  const [authenticated, setAuthenticated] = useRecoilState(authenticatedState);
+  const authenticated = useRecoilValue(authenticatedState);
   const resetAuthenticated = useResetRecoilState(authenticatedState);
   return (
     <Router>
       <Suspense fallback={<p>loading...</p>}>
         {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
-        <button
-          onClick={() => {
-            console.log(authenticated);
-          }}
-        >
-          check
-        </button>
 
         {authenticated ? (
           <button onClick={resetAuthenticated}>로그아웃</button>
         ) : (
-          <button onClick={() => {}}>로그인</button>
+          <Link to="/login">로그인</Link>
         )}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
-          <Route path="/workspace/*" element={<WorkSpace />} />
           <Route element={<AuthRoutes />}>
+            <Route path="/workspace/*" element={<WorkSpace />} />
             <Route path="/users" element={<Users />} />
           </Route>
         </Routes>
--- src/pages/auth/AuthRoutes.jsx
+++ src/pages/auth/AuthRoutes.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { Outlet, Navigate, useLocation, useNavigate } from "react-router-dom";
-import { useRecoilState, useRecoilValue } from "recoil";
+import React, { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { useRecoilValue, useResetRecoilState } from "recoil";
 import Login from "../Login";
 import { authenticatedState } from "../recoil/store";
 
 const AuthRoutes = () => {
-  let location = useLocation();
   const authenticated = useRecoilValue(authenticatedState);
-  console.log(location);
+  const resetAuthenticated = useResetRecoilState(authenticatedState);
+  useEffect(() => {
+    const validCheck = (token) => {
+      try {
+        const pureToken = token.split(" ")[1];
+        const payload = JSON.parse(window.atob(pureToken.split(".")[1]));
+        if (payload.exp * 1000 > Date.now()) {
+          return;
+        }
+        resetAuthenticated();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    const jwt = localStorage.getItem("login-token");
+    validCheck(jwt);
+  }, [resetAuthenticated]);
+
   return authenticated ? <Outlet /> : <Login />;
 };
 export default AuthRoutes;
