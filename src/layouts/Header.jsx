7a19d516b804f772d3f2fa72adca2e6000d8b8df
xanqus <sbk98031816@gmail.com>
2022-08-20 22:27:11 +0900

    header design ver 1.1

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRecoilState } from "recoil";
 import { authenticatedState } from "../recoil/store";
 import { Link } from "react-router-dom";
+import { BsPerson } from "react-icons/bs";
 
 const Header = () => {
   const [authenticated, setAuthenticated] = useRecoilState(authenticatedState);
@@ -76,8 +77,8 @@ const Header = () => {
             </a>
           </div>
           <div className="navbar-end flex grow-0 justify-end w-1/3">
-            <div className="w-2/5 hidden lg:flex">
-              <button v="btn btn-ghost btn-circle">
+            <div className="flex justify-end w-2/5 hidden lg:flex">
+              <button className="btn btn-ghost btn-circle mr-2">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -93,37 +94,46 @@ const Header = () => {
                   />
                 </svg>
               </button>
-              <button className="btn btn-ghost btn-circle z">
-                <div className="indicator">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    />
-                  </svg>
-                  <span className="badge badge-xs badge-primary indicator-item"></span>
-                </div>
-              </button>
+              {authenticated && (
+                <button className="btn btn-ghost btn-circle z">
+                  <div className="indicator">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                      />
+                    </svg>
+                    <span className="badge badge-xs badge-primary indicator-item"></span>
+                  </div>
+                </button>
+              )}
             </div>
             <div className="flex lg:w-3/5 w-full">
-              <div className="w-full">
+              <div className="flex justify-end items-center w-full">
                 {authenticated ? (
-                  <Link
-                    to="/"
-                    onClick={() => {
-                      setAuthenticated(false);
-                    }}
-                  >
-                    로그아웃
-                  </Link>
+                  <div className="w-full flex justify-end items-center">
+                    <Link to="#">
+                      <BsPerson size={25} strokeWidth={0} className="mr-5" />
+                    </Link>
+                    <div className="btn btn-sm bg-red-500 hover:bg-red-600 border-none">
+                      <Link
+                        to="/"
+                        onClick={() => {
+                          setAuthenticated(false);
+                        }}
+                      >
+                        로그아웃
+                      </Link>
+                    </div>
+                  </div>
                 ) : (
                   <div className="flex flex-row w-full h-full justify-end items-center">
                     <div className="lg:w-12 lg:btn-sm lg:rounded-md w-10 btn-xs text-xs btn btn-ghost btn-outline rounded whitespace-nowrap mx-1 lg:m-auto border-none hover:bg-transparent hover:text-gray-500">
